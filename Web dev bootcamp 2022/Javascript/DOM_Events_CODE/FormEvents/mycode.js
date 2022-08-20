663d85df33429c365d3fa61b84d07faf4c184e1b
okey <oke.anud.ben10@gmail.com>
2022-08-19 22:50:08 -0700

    little event work

--- Web dev bootcamp 2022/Javascript/DOM_Events_CODE/FormEvents/mycode.js
+++ Web dev bootcamp 2022/Javascript/DOM_Events_CODE/FormEvents/mycode.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('#shelterForm');
+const input = document.querySelector("#catName");
 form.addEventListener("submit", function(e) {
    e.preventDefault();
    console.log("SUBMITTED THE FORM");
